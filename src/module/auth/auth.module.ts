ba74563f4901b1c87d9eaf587c409b57854eb05e
Vansh Singh <vanshsin784@gmail.com>
2026-03-24 17:51:42 +0530

    fix: auth module

--- src/module/auth/auth.module.ts
+++ src/module/auth/auth.module.ts
@@ -1,11 +1,28 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
-
+import { UserService } from 'src/common/services/user.db.service';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from 'src/entities/user.entity';
+import { JwtStrategy } from './strategy/jwt.strategy';
+import { AuthResolver } from './auth.resolver';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [],
-  controllers: [],
-  providers: [AuthService],
+  imports: [
+    PassportModule,
+    TypeOrmModule.forFeature([User]),
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET', 'MARCH_2025'),
+        signOptions: { expiresIn: '7d' },
+      }),
+    }),
+  ],
+  providers: [AuthService, UserService, JwtStrategy, AuthResolver],
   exports: [AuthService],
 })
 export class AuthModule {}
